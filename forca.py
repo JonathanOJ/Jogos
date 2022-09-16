95b42e0076aee4f64e6554f7a24ecc3c29e9c5c8
Jonathan <jonathanjacobovski@alunos.utfpr.edu.br>
2022-09-16 19:30:17 -0300

    Jogos

--- forca.py
+++ forca.py
@@ -4,15 +4,29 @@ def jogar():
     print("**********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     while(not acertou and not enforcou):
-        print("Jogando...")
+
+        chute = input("Qual letra?")
+        chute = chute .strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+               letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("\nFim do jogo!")
 
-
+#asasas
 
 if(__name__ == "__main__"):
     jogar()
